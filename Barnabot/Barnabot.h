b2275356ea809c59c97692d6ab102aa45b9ccb77
mb2dev <bordage.mickael@gmail.com>
2017-01-31 00:23:10 +0100

    new project

---
+++ Barnabot/Barnabot.h
@@ -0,0 +1,19 @@
+//
+//  Barnabot.h
+//  Barnabot
+//
+//  Created by Mickael Bordage on 31/01/2017.
+//  Copyright © 2017 Mickael Bordage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Barnabot.
+FOUNDATION_EXPORT double BarnabotVersionNumber;
+
+//! Project version string for Barnabot.
+FOUNDATION_EXPORT const unsigned char BarnabotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Barnabot/PublicHeader.h>
+
+
